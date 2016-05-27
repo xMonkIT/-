8e9e9db85ff0ef482092ae48f29787ea7ab6b824
xMonkIT <omelin.vladimir@gmail.com>
2016-05-27 14:46:04 +0300

    add test of simple factory

--- lab9/Program.cs
+++ lab9/Program.cs
@@ -1,11 +1,48 @@
-﻿namespace lab9
+﻿using System;
+using lab9.Factory.SimpleFactory;
+
+namespace lab9
 {
     static class Program
     {
         static void Main(string[] args)
         {
-            //Здесь нужно заказывать различную пиццу различных стилей
+            while (true)
+            {
+                Console.Write("Выберите магазин: New York (1), Chicago (2) [default (1)]: ");
+                var input = Console.ReadLine();
 
+                IPizzaSimpleFactory factory;
+
+                switch (input)
+                {
+                    case "":
+                    case "1":
+                        factory = new NYPizzaSimpleFactory();
+                        break;
+                    case "2":
+                        factory = new ChickagoPizzaSimpleFactory();
+                        break;
+                    default:
+                        continue;
+                }
+
+                var store = new PizzaStore(factory);
+
+                Console.Write("Введите название пиццы: ");
+                input = Console.ReadLine();
+
+                try
+                {
+                    store.OrderPizza(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
