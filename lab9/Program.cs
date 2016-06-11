49fbf6972fdb1868441cb69b0cbcf787b1e3deb4
xMonkIT <omelin.vladimir@gmail.com>
2016-06-11 18:19:52 +0300

    added testing factory method

--- lab9/Program.cs
+++ lab9/Program.cs
@@ -1,11 +1,58 @@
 ﻿using System;
+using lab9.Factory.FactoryMethod;
 using lab9.Factory.SimpleFactory;
 
 namespace lab9
 {
-    static class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+        {
+            //TestSimpleFactory();
+            TestFactoryMethod();
+        }
+
+        private static void TestFactoryMethod()
+        {
+            while (true)
+            {
+                Console.Write("Выберите магазин: New York (1), Chicago (2) [default (1)]: ");
+                var input = Console.ReadLine();
+
+                Factory.FactoryMethod.PizzaStore store;
+
+                switch (input)
+                {
+                    case "":
+                    case "1":
+                        store = new NYPizzaStore();
+                        break;
+                    case "2":
+                        store = new ChickagoPizzaStore();
+                        break;
+                    default:
+                        continue;
+                }
+
+                Console.WriteLine($"Доступные виды пиццы:\n{string.Join("\n", store.GetMenu())}\n");
+
+                Console.Write("Введите название пиццы: ");
+                input = Console.ReadLine();
+
+                try
+                {
+                    store.OrderPizza(input);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void TestSimpleFactory()
         {
             while (true)
             {
@@ -27,7 +74,7 @@ namespace lab9
                         continue;
                 }
 
-                var store = new PizzaStore(factory);
+                var store = new Factory.SimpleFactory.PizzaStore(factory);
 
                 Console.WriteLine($"Доступные виды пиццы:\n{string.Join("\n", store.GetMenu())}\n");
 
